650498cdc34ed6ff09ec13a384e47b77fe619230
Matthew Ching <matthewaching@gmail.com>
2022-07-15 00:04:19 -0500

    Change grid basis from flex to grid
    There were issues regarding the use of line break divs while using
    display flex. These issues should be alleviated with using display grid.

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const container = document.querySelector('.container');
 
 // Allows for a changing square template. If a rectangular design is eventually decided on, sqrt code will need to change.
-const oneDimension = 100;
+const oneDimension = 10;
 const resolution = oneDimension ** 2; 
 
 for (let i = 1; i <= resolution; i++) {
-    if ((i-1) % Math.sqrt(resolution) === 0) {
-        const breakSquare = document.createElement('div');
-        breakSquare.setAttribute('class', 'break');
-        container.appendChild(breakSquare);
-    }
     const divSquare = document.createElement('div');
     divSquare.setAttribute('class', 'square');
     divSquare.addEventListener('mouseover', () => {
