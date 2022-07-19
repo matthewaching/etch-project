e409d14340c391b09b31d1809572af8f7b4b3903
Matthew Ching <matthewaching@gmail.com>
2022-07-18 23:22:53 -0500

    Implement keyboard control with responsive design
    User is now able to draw using the keyboard arrow keys. Future edit
    should address input repetition when the key is held down.

--- script.js
+++ script.js
@@ -6,25 +6,97 @@ const container = document.querySelector('.container');
 
 const resolution = 128 * 72;
 
-for (let i = 1; i <= resolution; i++) {
-    const divSquare = document.createElement('div');
-    divSquare.setAttribute('class', 'square');
-    divSquare.addEventListener('mouseover', () => {
-        divSquare.classList.add('activated');
-    });
-    container.appendChild(divSquare);
+const currentPixel = {
+    'x': 1,
+    'y': 1,
+}
+
+for (let i = 72; i >= 1; i--) {
+    for (let j = 1; j <= 128; j++){
+        const divSquare = document.createElement('div');
+        divSquare.setAttribute('class', 'square');
+        divSquare.addEventListener('mouseover', () => {
+            divSquare.classList.add('activated');
+        });
+        divSquare.setAttribute('data-x', j);
+        divSquare.setAttribute('data-y', i);
+        container.appendChild(divSquare);
+    }
 }
 
 // document.addEventListener("keydown", function (e) {
 
 // })
 
-// const arrowArray = [...document.querySelectorAll('.arrow')];
-// for (let arrow of arrowArray)
-// const lArr = document.querySelector('#l-ar');
-// const rArr = document.querySelector('#r-ar');
-// const uArr = document.querySelector('#u-ar');
-// const dArr = document.querySelector('#d-ar');
+const lArr = document.querySelector('#l-ar');
+const rArr = document.querySelector('#r-ar');
+const uArr = document.querySelector('#u-ar');
+const dArr = document.querySelector('#d-ar');
+
+document.addEventListener('keydown', e => {
+    if (e.keyCode === 37) {
+        lArr.classList.add('pressed');
+
+        if (currentPixel.x > 1) {
+            currentPixel.x--;
+            runPixel(currentPixel.x, currentPixel.y);
+        }
+    }
+
+    if (e.keyCode === 38) {
+        uArr.classList.add('pressed');
+       
+        if (currentPixel.y < 72) {
+            currentPixel.y++;
+            runPixel(currentPixel.x, currentPixel.y);
+        }
+    }
+
+    if (e.keyCode === 39) {
+        rArr.classList.add('pressed');
+        
+        if (currentPixel.x < 128) {
+            currentPixel.x++;
+            runPixel(currentPixel.x, currentPixel.y);
+        }
+    }
+
+    if (e.keyCode === 40) {
+        dArr.classList.add('pressed');
+        
+        if (currentPixel.y > 1) {
+            currentPixel.y--;
+            runPixel(currentPixel.x, currentPixel.y);
+        }
+    }
+});
+
+document.addEventListener('keyup', e => {
+    if (e.keyCode === 37) {
+        lArr.classList.remove('pressed');
+    }
+
+    if (e.keyCode === 38) {
+        uArr.classList.remove('pressed');
+    }
+
+    if (e.keyCode === 39) {
+        rArr.classList.remove('pressed');
+    }
+
+    if (e.keyCode === 40) {
+        dArr.classList.remove('pressed');
+    }
+});
+
+
+
+function runPixel(x, y) {
+    const findPixel = document.querySelector(`[data-x='${x}'][data-y='${y}']`);
+    findPixel.classList.add('activated');
+}
+
+runPixel(currentPixel.x, currentPixel.y);
 
 // function Keycoder (arrow, keyCode) {
 //     this.arrow = arrow;
